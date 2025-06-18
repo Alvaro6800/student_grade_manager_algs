c096746d0fc7d127f67105edce975baf1efb0aa8
Alvaro6800 <alvarolgsouza2009@gmail.com>
2025-06-18 13:13:30 -0300

    PUT and DELETE inserted

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const server = http.createServer((request, response) => {
     })
 
     request.on('end', () => {
+        const id = url.split('/')[2]
+
         if(url === '/grades' && method === 'GET'){
             response.writeHead(200, {'content-type': 'application/json'})
             response.end(JSON.stringify(grades))
@@ -28,6 +30,33 @@ const server = http.createServer((request, response) => {
             grades.push(newGrade)
             response.writeHead(201, {'content-type': 'application/json'})
             response.end(JSON.stringify(newGrade))
+        }
+        else if(url.startsWith('/grades/') && method === 'PUT'){
+            const {studentName, subject, grade} = JSON.parse(body);
+            const gradeToUpdate = grades.find(g => g.id === id)
+            if(gradeToUpdate){
+                gradeToUpdate.studentName = studentName
+                gradeToUpdate.subject = subject
+                gradeToUpdate.grade = grade
+                response.writeHead(200, {'content-type': 'application/json'})
+                response.end(JSON.stringify(gradeToUpdate))
+            }
+            else{
+                response.writeHead(404, {'content-type': 'application/json'})
+                response.end(JSON.stringify({message: 'Grade not found'}))
+            }
+        }
+        else if(url.startsWith('/grades/') && method === 'DELETE'){
+            const index = grades.findIndex(g => g.id === id)
+            if(index !== -1){
+                grades.splice(index, 1)
+                response.writeHead(204);
+                response.end();
+            }
+            else{
+                response.writeHead(404, {'content-type': 'application/json'})
+                response.end(JSON.stringify({message: 'Grade not found'}))
+            }
         }
         else{
             response.writeHead(404, {'content-type': 'application/json'})
